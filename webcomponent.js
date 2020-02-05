2198159480652f6250d6e0cf7318ebc0f3b660bc
victors1987 <60398987+victors1987@users.noreply.github.com>
2020-02-05 15:55:45 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -3,25 +3,6 @@
     tmpl.innerHTML = `
         <h1>Hola everis</h1>
 		
-		<script>
-			window.setInterval (BlinkIt, 500);
-			var color = "red";
-			function BlinkIt () {
-			var blink = document.getElementById ("blink");
-			color = (color == "#ffffff")? "red" : "#ffffff";
-			blink.style.color = color;
-			blink.style.fontSize='36px';}
-			</script>
-			<div id="blink">Hola!</div>
-		
-			Hola!</div>
-		
-		
-		
-		
-		
-		
-		
 		
     `;
 
